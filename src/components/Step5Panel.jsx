2afc3a882f0f172ee8439559004a77b9a5cbb0af
coderalchemy24 <eo.69.mzx@google.com>
2026-03-28 20:55:22 +0100

    Change image-urls

--- src/components/Step5Panel.jsx
+++ src/components/Step5Panel.jsx
@@ -5,7 +5,7 @@ export default function Step5Panel({step}) {
 
 return (
         <div className="form-step-5">
-            <img src={'../assets/images/icon-thank-you.svg'} alt="thank you" className="thank-you"/>
+            <img src={'/assets/images/icon-thank-you.svg'} alt="thank you" className="thank-you"/>
             <h1 className="title">Thank you!</h1>
             <p className="description">
               Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.
